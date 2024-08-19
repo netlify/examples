948da72b0bd1fc0c2130e3bea53c0dcf93936c4b
Phil Hawksworth <phil@netlify.com>
2024-08-19 18:53:37 -0400

    bump the build for testing

--- examples/ai-bot-control/netlify/edge-functions/bots.ts
+++ examples/ai-bot-control/netlify/edge-functions/bots.ts
@@ -2,19 +2,15 @@ import { Context, Config } from "@netlify/edge-functions";
 import agents from "../../agents.json" with { type: "json" };
 
 export default async (request: Request, context: Context) => {
-
   const ua = request.headers.get('user-agent');
   let isBot = false;
-
   agents.forEach(u => {
     if (ua.toLowerCase().includes(u.toLowerCase())) {
       isBot = true;
     }
   })
-
   const response = isBot ? new Response(null, { status: 401 }) : await context.next();
   return response;
-
 };
 
 export const config: Config = {
