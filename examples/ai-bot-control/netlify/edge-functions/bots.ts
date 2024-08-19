1b287107a8d6991e1f5747faed7d82290c1a4fa4
Phil Hawksworth <phil@netlify.com>
2024-08-19 18:02:59 -0400

    use ts for the EF

--- examples/ai-bot-control/netlify/edge-functions/bots.ts
+++ examples/ai-bot-control/netlify/edge-functions/bots.ts
@@ -1,11 +1,11 @@
-// Source a list of know User Agent strings used by AI crawlers.
-const agents = require('../../agents.json');
+import { Context, Config } from "@netlify/edge-functions";
+import agents from "../../agents.json" with { type: "json" };
 
+export default async (request: Request, context: Context) => {
 
-export default async (request, context) => {
   const ua = request.headers.get('user-agent');
   let isBot = false;
-  
+
   agents.forEach(u => {
     if (ua.toLowerCase().includes(u.toLowerCase())) {
       isBot = true;
@@ -14,4 +14,12 @@ export default async (request, context) => {
 
   const response = isBot ? new Response(null, { status: 401 }) : await context.next();
   return response;
+
 };
+
+export const config: Config = {
+  path: "*",
+};
+
+
+
