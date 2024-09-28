d7defcc00b6b42426b320aea1c4176edb9cec996
Sean C Davis <scdavis41@gmail.com>
2024-09-28 08:32:48 -0400

    Replace supabase example

--- examples/supabase-astro/src/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-import type { AstroCookieSetOptions } from "astro";
-
-export const DEFAULT_COOKIE_OPTIONS: AstroCookieSetOptions = {
-  path: "/",
-  sameSite: "strict",
-  httpOnly: true,
-  secure: import.meta.env.NODE_ENV === "production",
-};
--- examples/supabase-astro/src/utils/database.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_DATABASE_URL as string
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
