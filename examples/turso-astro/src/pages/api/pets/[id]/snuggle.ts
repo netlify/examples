f2d90d58b1f2a6d16267932e9993492f64a87684
NikAiyer <nick.aiyer@gmail.com>
2024-09-30 10:59:51 -0700

    updated files

--- examples/turso-astro/src/pages/api/pets/[id]/snuggle.ts
+++ examples/turso-astro/src/pages/api/pets/[id]/snuggle.ts
@@ -1,17 +1,30 @@
-import { addSnuggle, getPet } from '@/utils/apiHelpers'
-import { petPath } from '@/utils/routes'
 import type { APIRoute } from 'astro'
+import { turso } from '../../../../utils/database'
 
 export const prerender = false
 
 export const POST: APIRoute = async ({ params, redirect, cookies, request }) => {
-  const pet = await getPet(parseInt(params.id as string))
+  const { id } = params
+  if (!id) {
+    return new Response('No pet id provided', { status: 400 })
+  }
+
+  const data = await turso.execute({ sql: `SELECT * FROM pets WHERE id = ?`, args: [id] })
+
+  if (data.rows.length === 0) {
+    console.error(`No pet found with id ${id}`)
+    return
+  }
+  const pet = data.rows[0]
 
   if (!pet) {
     return new Response('Where`d that pet go?', { status: 404 })
   }
 
-  await addSnuggle(pet)
+  await turso.execute({
+    sql: `UPDATE pets SET snuggles = snuggles + 1 WHERE id = ?`,
+    args: [pet.id],
+  })
 
-  return redirect(petPath(pet), 303)
+  return redirect(`/pets/${pet.id}`, 303)
 }
--- examples/turso-astro/src/utils/database.ts
+++ examples/turso-astro/src/utils/database.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@libsql/client'
 
+const tursoUrl = process.env.TURSO_DATABASE_URL as string
+const tursoAuthToken = process.env.TURSO_AUTH_TOKEN as string
+
 export const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL as string,
-  authToken: process.env.TURSO_AUTH_TOKEN as string,
+  url: tursoUrl,
+  authToken: tursoAuthToken,
 })
