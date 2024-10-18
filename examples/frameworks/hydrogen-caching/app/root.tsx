8d8014b9868081d82d5b3ecfcbe7a5236fc531d3
Philippe Serhal <philippe.serhal@netlify.com>
2024-10-18 15:50:39 -0400

    feat: add full page caching
    Cache most pages for 1 hour on the Netlify CDN with stale-while-revalidate (allowing 1 min for
    background revalidation to complete). Cache these also on browsers (and anything else upstream) but
    require a conditional request to revalidate.
    
    On the search page, vary on the search input.
    
    Disable this caching on data loaders that have personalization: account/*, cart.

--- examples/frameworks/hydrogen-caching/app/root.tsx
+++ examples/frameworks/hydrogen-caching/app/root.tsx
@@ -1,5 +1,9 @@
 import {useNonce, getShopAnalytics, Analytics} from '@shopify/hydrogen';
-import {defer, type LoaderFunctionArgs} from '@netlify/remix-runtime';
+import {
+  defer,
+  type HeadersFunction,
+  type LoaderFunctionArgs,
+} from '@netlify/remix-runtime';
 import {
   Links,
   Meta,
@@ -17,6 +21,11 @@ import appStyles from '~/styles/app.css?url';
 import {PageLayout} from '~/components/PageLayout';
 import {CartProvider, useCart} from '~/components/CartProvider';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
+import {CACHE_1_HOUR_SWR} from '~/lib/page-cache';
+
+export const headers: HeadersFunction = () => ({
+  ...CACHE_1_HOUR_SWR,
+});
 
 export type RootLoader = typeof loader;
 
