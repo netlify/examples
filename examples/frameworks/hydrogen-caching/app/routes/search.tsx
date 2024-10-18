464e804fc6175d36b2cef5100dab8d1391f05f27
Philippe Serhal <philippe.serhal@netlify.com>
2024-10-18 15:50:39 -0400

    feat: add full page caching
    Cache most pages for 1 hour on the Netlify CDN with stale-while-revalidate (allowing 1 min for
    background revalidation to complete). Cache these also on browsers (and anything else upstream) but
    require a conditional request to revalidate.
    
    On the search page, vary on the search input.
    
    Disable this caching on data loaders that have personalization: account/*, cart.

--- examples/frameworks/hydrogen-caching/app/routes/search.tsx
+++ examples/frameworks/hydrogen-caching/app/routes/search.tsx
@@ -2,11 +2,13 @@ import {
   json,
   type LoaderFunctionArgs,
   type ActionFunctionArgs,
+  type HeadersFunction,
 } from '@netlify/remix-runtime';
 import {useLoaderData, type MetaFunction} from '@remix-run/react';
 import {getPaginationVariables, Analytics} from '@shopify/hydrogen';
 import {SearchForm} from '~/components/SearchForm';
 import {SearchResults} from '~/components/SearchResults';
+import {CACHE_1_HOUR_SWR} from '~/lib/page-cache';
 import {
   type RegularSearchReturn,
   type PredictiveSearchReturn,
@@ -16,6 +18,11 @@ import {
 export const meta: MetaFunction = () => {
   return [{title: `Hydrogen | Search`}];
 };
+
+export const headers: HeadersFunction = ({parentHeaders, loaderHeaders}) => ({
+  ...Object.fromEntries(parentHeaders),
+  ...Object.fromEntries(loaderHeaders),
+});
 
 export async function loader({request, context}: LoaderFunctionArgs) {
   const url = new URL(request.url);
@@ -29,7 +36,12 @@ export async function loader({request, context}: LoaderFunctionArgs) {
     return {term: '', result: null, error: error.message};
   });
 
-  return json(await searchPromise);
+  return json(await searchPromise, {
+    headers: {
+      ...CACHE_1_HOUR_SWR,
+      'Netlify-Vary': 'query=q|limit|predictive',
+    },
+  });
 }
 
 /**
