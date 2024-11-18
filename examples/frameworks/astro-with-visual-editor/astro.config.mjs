ba516726627823d46c58886837fe311b0a645599
TomasBankauskas <tbdizainas@gmail.com>
2024-11-18 09:55:22 +0200

    added example astro-with-visual-editor

---
+++ examples/frameworks/astro-with-visual-editor/astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	markdown: {
+		shikiConfig: {
+		  theme: 'material-theme-ocean',
+		},
+	  },
+	integrations: [mdx(), sitemap()],
+	server: {
+		port: 3000,
+	  },
+});
