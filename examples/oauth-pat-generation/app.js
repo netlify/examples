26a9bd910a1229b213077d8ceb150ea806278884
Domitrius Clark <domitriusaclark@gmail.com>
2025-03-18 12:36:21 -0400

    Add new example for oauth2 & pat generation

---
+++ examples/oauth-pat-generation/app.js
@@ -0,0 +1,93 @@
+const CLIENT_ID = "A664Jlyp4mpBSVIktWtGjeshNaERA_CBzSC_IcNl618";
+const REDIRECT_URI = "http://localhost:8888/";
+
+function generateState() {
+  return crypto.randomUUID(); // Simple and secure way to generate state
+}
+
+function handleNetlifyOAuth() {
+  const clientId = CLIENT_ID;
+  const redirectUri = REDIRECT_URI;
+  const state = generateState();
+  
+  // Store state in sessionStorage (more secure than localStorage for OAuth states)
+  sessionStorage.setItem('oauth_state', state);
+  
+  const authLink = `https://app.netlify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&state=${state}`;
+
+  window.location.href = authLink;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const oAuthButton = document.querySelector("#oauth-button");
+  if (oAuthButton) {
+    oAuthButton.addEventListener("click", handleNetlifyOAuth);
+  } else {
+    console.error('OAuth button not found');
+  }
+});
+
+const hash = document.location.hash;
+
+/*
+ * This function is called when a user returns from Netlify and has accepted the
+ * request to authorize your app.
+ *
+ * It extracts the token from the response and use it to do a simple API request
+ * fetching the latest sites from the user from Netlify.
+ */
+function handleAccessToken() {  
+  // The access token is returned in the hash part of the document.location
+  //   #access_token=1234&response_type=token&state=...
+  const response = hash.replace(/^#/, '').split('&').reduce((result, pair) => {
+    const keyValue = pair.split('=');
+    result[keyValue[0]] = keyValue[1];
+    return result;
+  }, {});
+
+  // Verify state parameter
+  const savedState = sessionStorage.getItem('oauth_state');
+  // Clean up immediately
+  sessionStorage.removeItem('oauth_state');
+
+  if (!savedState || savedState !== response.state) {
+    console.log('Security Error: Invalid state parameter. Possible CSRF attack.');
+    return;
+  }
+
+  // Remove the token so it's not visible in the URL after we're done
+  document.location.hash = '';
+
+  // Updated to use our Netlify function
+  fetch('/create-token', {
+    method: "POST",
+    headers: {
+      'Authorization': `Bearer ${response.access_token}`,
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+    })
+    .catch(error => {
+      console.log(error)
+    });
+}
+
+/*
+ * The Oauth2 implicit grant flow works by sending the user to Netlify where she'll
+ * be asked to grant authorization to your application. Netlify will then redirect
+ * back to the Redirect URI on file for your app and set an access_token paramter
+ * in the "hash" part of the URL.
+ *
+ * If we have any hash, it's because the user is coming back from Netlify and we
+ * can start doing API requests on their behalf.
+ *
+ * If not, we'll trigger the first step and prepare to send the user to Netlify.
+ */
+if (document.location.hash) {
+  handleAccessToken();
+}
+
+
