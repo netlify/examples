0646ffe6da05dce5266b9c622e12027d37971ecf
Jake Champion <me@jakechampion.name>
2025-10-22 11:17:04 +0100

    Update log.ts

--- examples/edge-functions/netlify/edge-functions/log.ts
+++ examples/edge-functions/netlify/edge-functions/log.ts
@@ -3,9 +3,7 @@ import type { Context, Config } from "@netlify/edge-functions";
 export default async (request: Request, context: Context) => {
   console.log("Hello from the logging service");
 
-  return new Response("The request to this URL was logged", {
-    headers: { "content-type": "text/plain" },
-  });
+  return new Response("The request to this URL was logged");
 };
 
 export const config: Config = {
