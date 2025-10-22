e08ed767e6be518e9ed5d9559a13055f17cea06a
Jake Champion <me@jakechampion.name>
2025-10-22 11:14:21 +0100

    Change response content type to text/plain

--- examples/edge-functions/netlify/edge-functions/log.ts
+++ examples/edge-functions/netlify/edge-functions/log.ts
@@ -4,7 +4,7 @@ export default async (request: Request, context: Context) => {
   console.log("Hello from the logging service");
 
   return new Response("The request to this URL was logged", {
-    headers: { "content-type": "text/html" },
+    headers: { "content-type": "text/plain" },
   });
 };
 
