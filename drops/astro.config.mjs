60e4ea136eaa99a7031d128a557a8c019162d3e1
Sean C Davis <scdavis41@gmail.com>
2025-12-05 10:50:04 -0500

    Add netlify adapter

--- drops/astro.config.mjs
+++ drops/astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 
+import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
